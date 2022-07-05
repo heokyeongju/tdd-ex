5b2b1b6dcf967c6c553e456673b528999373f55b
heokyeongju <beewt@naver.com>
2022-07-06 08:08:40 +0900

    add 메소드 구현

--- src/test/java/com/ll/exam/Apptest.java
+++ src/test/java/com/ll/exam/Apptest.java
@@ -3,17 +3,22 @@ package com.ll.exam;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Apptest {
     @Test
     public void 더하기_테스트(){
-        int rs = Calculator.add("10+20");
-        assertEquals(30,rs);
+        int rs = Calculator.add("10 + 20");
+        assertEquals(30, rs);
     }
     @Test
     public void 더하기_테스트_2(){
-        int rs = Calculator.add("50+20");
+        int rs = Calculator.add("50 + 20");
         assertEquals(70,rs);
+    }
+
+    @Test
+    public void 더하기_테스트_3(){
+        int rs = Calculator.add("100 + 20");
+        assertEquals(120,rs);
     }
 }
