4d71d91570cd390e14ddb9af1f03117d89d28d9f
heokyeongju <beewt@naver.com>
2022-07-06 08:35:37 +0900

    multiply method and test case added

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -3,26 +3,38 @@ package com.ll.exam;
 public class Calculator {
     public static int calc(String s){
         boolean isSubtract = s.indexOf(" - ") != -1;
+        boolean isMultiply = s.indexOf(" * ") != -1;
 
         if (isSubtract) {
             return subtract(s);
+        }
+        else if(isMultiply){
+            return multiply(s);
         }
 
         return add(s);
     }
 
-    public static int add(String s){
+    private static int add(String s){
         String[] sBits = s.split(" \\+ ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
 
         return no1+no2;
     }
-    public static int subtract(String s){
+    private static int subtract(String s){
         String[] sBits = s.split(" \\- ");
         int no1 = Integer.parseInt(sBits[0]);
         int no2 = Integer.parseInt(sBits[1]);
 
         return no1-no2;
     }
+    private static int multiply(String s) {
+        String[] sBits = s.split(" \\* ");
+        int no1 = Integer.parseInt(sBits[0]);
+        int no2 = Integer.parseInt(sBits[1]);
+
+        return no1 * no2;
+    }
+
 }
--- src/test/java/com/ll/exam/Apptest.java
+++ src/test/java/com/ll/exam/Apptest.java
@@ -7,24 +7,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Apptest {
     @Test
     public void 더하기_테스트(){
-        int rs = Calculator.add("10 + 20");
+        int rs = Calculator.calc("10 + 20");
         assertEquals(30, rs);
     }
     @Test
     public void 더하기_테스트_2(){
-        int rs = Calculator.add("50 + 20");
+        int rs = Calculator.calc("50 + 20");
         assertEquals(70,rs);
     }
 
     @Test
     public void 더하기_테스트_3(){
-        int rs = Calculator.add("100 + 20");
+        int rs = Calculator.calc("100 + 20");
         assertEquals(120,rs);
     }
 
     @Test
     public void 빼기_테스트_1(){
-        int rs = Calculator.subtract("100 - 20");
+        int rs = Calculator.calc("100 - 20");
         assertEquals(80,rs);
+    }
+    @Test
+    public void 곱하기_테스트_1(){
+        int rs = Calculator.calc("10 * 20");
+        assertEquals(200,rs);
     }
 }
